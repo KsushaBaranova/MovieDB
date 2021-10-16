4f130a7c64806f2c20d00450e631431d6f512982
KsushaBaranova <baranova9797@gmail.com>
2021-10-16 20:31:27 +0300

    Add dropdown to trending screen

--- MovieDB/src/services/apiFilm.ts
+++ MovieDB/src/services/apiFilm.ts
@@ -2,16 +2,17 @@ import {TrendingFilmResponse} from '../interfaces';
 import {request, RequestType} from './apiManager';
 
 export interface FilmApiInterface<T> {
-  fetchTrendingDefault(): Promise<T>;
+  fetchTrendingDefault(orderBy: string[]): Promise<T>;
 }
 
 class FilmApi<T> implements FilmApiInterface<T> {
   private token: string =
     'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MTM2NmIzM2IyY2IzZDU0ZGY5ZDgxZGZiYWUxYWRlMyIsInN1YiI6IjYxNjQ4YTJiOGM0MGY3MDAyYTc0MDFiOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nRzDNXNo9CxvJClIqjwYzheIgU80WObEswCn4ypjfnM';
 
-  async fetchTrendingDefault(): Promise<T> {
+  async fetchTrendingDefault(orderBy: string[]): Promise<T> {
     return request<T>(RequestType.fetchTrendingDefault, {
       token: this.token,
+      params: orderBy,
     });
   }
 }
