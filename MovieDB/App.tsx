46ff8d6313ed95c1e2d775c7fec8130ec00ce969
KsushaBaranova <baranova9797@gmail.com>
2021-10-17 18:32:42 +0300

    add navigation

--- MovieDB/App.tsx
+++ MovieDB/App.tsx
@@ -3,9 +3,22 @@ import {store} from './src/redux/store';
 import {Provider} from 'react-redux';
 import TrendingScreen from './src/screens/TrendingScreen';
 import SearchScreen from './src/screens/SearchScreen';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
 
-const App: React.FC<{}> = () => {
-  return <Provider store={store}>{<TrendingScreen />}</Provider>;
-};
+const Tab = createBottomTabNavigator();
+
+function App() {
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <Tab.Navigator screenOptions={{headerShown: false}}>
+          <Tab.Screen name="TrendingScreen" component={TrendingScreen} />
+          <Tab.Screen name="SearchScreen" component={SearchScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
+}
 
 export default App;
